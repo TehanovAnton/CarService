fc6179da14f8293f96c77bd28393e2ea8d3c13ab
anton <tehanovanton@gmail.com>
2021-07-02 00:15:28 +0300

    rename column  maill adress to email

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_203322) do
+ActiveRecord::Schema.define(version: 2021_07_01_204151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_01_203322) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
-    t.string "mail_adress"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
