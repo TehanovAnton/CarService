5ff071fe1d2921f9728fa1e3cb47c618ac6be52b
anton <tehanovanton@gmail.com>
2021-07-02 19:43:56 +0300

    user/index with only email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/users', to: 'user#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
