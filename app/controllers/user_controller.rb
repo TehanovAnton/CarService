e45b8985729dbe40ac7897a65d8b9065370ec36b
anton <tehanovanton@gmail.com>
2021-07-03 00:35:52 +0300

    user#show

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,13 @@ class UserController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def delete
+    @user = User.find(params[:id])
+    @user.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   get '/users', to: 'user#index'
+  get '/users/:id', to: 'user#show'
+  delete 'users/:id', to: 'user#delete'
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
