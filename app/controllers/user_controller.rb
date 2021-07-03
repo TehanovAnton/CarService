b7d5b88983e6f8e660df705a7df439971eafc924
anton <tehanovanton@gmail.com>
2021-07-03 16:20:31 +0300

    #show_me check if user authoriяed done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
   def show
     # проверить id
     exists_id = User.all.map { |el| el.id }.include? params[:id].to_i
-    
+
     if exists_id
       @user = User.find(params[:id])
     else
@@ -15,13 +15,12 @@ class UserController < ApplicationController
   end
 
   def me
+    redirect_to new_user_session_path if current_user.nil?
     @me = current_user
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-
 
     redirect_to user_index_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user, only: [:index, :show, :destroy]
-
   devise_for :users
+
+  resources :user, only: [:index, :show, :destroy]
   get '/me', to: 'user#me'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
