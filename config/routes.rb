dc21546abfb401b2d111d99cd0c712a17ef99f78
anton <tehanovanton@gmail.com>
2021-07-07 21:57:54 +0300

    update authorized user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :user, only: %i[index show destroy]
   get '/me', to: 'user#me'
+  get '/edit', to: 'user#edit'
+  put '/edit/:id', to: 'user#update'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
