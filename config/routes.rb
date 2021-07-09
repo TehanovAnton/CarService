bd332aa98f845850f4ae5e0bd83fe68813d76e83
anton <tehanovanton@gmail.com>
2021-07-09 16:08:36 +0300

    demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  devise_for :users
-
-  resources :user, only: %i[index show destroy]
-  get '/me', to: 'user#me'
-  get '/edit', to: 'user#edit'
-  put '/edit/:id', to: 'user#update'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
