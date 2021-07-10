539e8bf017d51758ad7e41ecd75469277591e05c
anton <tehanovanton@gmail.com>
2021-07-10 22:56:04 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'user#index'
+
+  devise_for :users
+
+  resources :user, only: %i[index show destroy]
+  get '/me', to: 'user#me'
+  get '/edit', to: 'user#edit'
+  put '/edit/:id', to: 'user#update'
 end
