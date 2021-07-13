abee7ccc17c53a6f950640fc5a12308bdb4a760d
anton <tehanovanton@gmail.com>
2021-07-13 14:48:37 +0300

    admin can edit all users by id

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :user, only: %i[index show destroy]
-  resources :admin
+  resources :admin, only: %i[index edit update]
   get '/me', to: 'user#me'
   get '/edit', to: 'user#edit'
   put '/edit/:id', to: 'user#update'
+
+  # get '/admin/edit', to: 'admin#edit'
+  # put '/admin/edit/:id', to: 'user#update'
 end
