0f958b78fd83a82f2cc2909b7be0ae667049dbaf
anton <tehanovanton@gmail.com>
2021-07-14 18:41:14 +0300

    errors controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   resources :user, only: %i[index show destroy]
   resources :admin, only: %i[index edit update]
+  resources :errors, only: %i[index]
   get '/me', to: 'user#me'
   get '/edit', to: 'user#edit'
   put '/edit/:id', to: 'user#update'
+
+  match '*path', to: 'errors#index', via: :all
 end
