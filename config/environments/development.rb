b250f07166dd8ee07d1dfbc309f0acf6f444312b
anton <tehanovanton@gmail.com>
2021-07-20 20:57:19 +0300

    smtp settings development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   # -----------
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: :http }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "carserviceror@gmail.com",
+    password: "ewqqwewsx", 
+    domain: "smtp.gmail.com",
+    openssl_verify_mode: "none",
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
