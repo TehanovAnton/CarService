7ac5e55baa4dfea7f8aa13e62fc987c20ee1e471
anton <tehanovanton@gmail.com>
2021-07-29 23:08:29 +0300

    orders #delete

--- app/controllers/service_orders_controller.rb
+++ app/controllers/service_orders_controller.rb
@@ -1,13 +1,26 @@
 class ServiceOrdersController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
+    @service_orders = @user.service_orders
+  end
+
+  def new
+    @user = User.find_by(id: params[:user_id])
   end
 
   def create
     @user = User.find_by(id: params[:user_id])
     @user.service_orders.create(service_orders_params)
+    redirect_to user_service_orders_path(@user)
+  end
 
-    redirect_to user_service_orders(@user)
+  def destroy
+    @user = User.find_by(id: params[:user_id])
+
+    puts "params_id: #{params[:user_id]}"
+
+    ServiceOrder.find_by(id: params[:id]).destroy
+    redirect_to user_service_orders_path(@user)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :service_orders
+  has_many :service_orders, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
