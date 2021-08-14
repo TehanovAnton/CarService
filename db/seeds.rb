f71a212cad1dc501a29a0ac9a90e2c92acff4ba9
anton <tehanovanton@gmail.com>
2021-08-14 19:35:19 +0300

    new order mechanic drop down

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create(first_name: 'admin', last_name: 'adminov', phone_number: '911', email: 'admin@gmail.com', password: 'ewqqwe')
+
+Mechanic.create(first_name: 'Wilson', last_name: 'mech', phone_number: '911', email: 'wilsonmech@gmail.com', password: 'ewqqwe')
+Mechanic.create(first_name: 'Koner', last_name: 'mech', phone_number: '191', email: 'Konermech@gmail.com', password: 'ewqqwe')
+Mechanic.create(first_name: 'Rayn', last_name: 'mech', phone_number: '119', email: 'Raynmech@gmail.com', password: 'ewqqwe')
