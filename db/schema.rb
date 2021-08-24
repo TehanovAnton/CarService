0e454b3fc3164d3f7031082614220156974774d6
anton <tehanovanton@gmail.com>
2021-08-25 00:19:11 +0300

    users type default changed on Client

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_080029) do
+ActiveRecord::Schema.define(version: 2021_08_24_211449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_clients_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+  end
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -59,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_08_22_080029) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "type", default: "User", null: false
+    t.string "type", default: "Client", null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
