836098cff2a78695ffdd4d4a9b8156bafef5c251
anton <tehanovanton@gmail.com>
2021-08-25 11:21:28 +0300

    model work

---
+++ app/models/mechanic_service.rb
@@ -0,0 +1,4 @@
+class MechanicService < ApplicationRecord
+  belongs_to :mechanic
+  belongs_to :service
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_065740) do
+ActiveRecord::Schema.define(version: 2021_08_25_080037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_08_25_065740) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_clients_on_email", unique: true
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+  end
+
+  create_table "mechanic_services", force: :cascade do |t|
+    t.integer "mechanic_id", null: false
+    t.integer "service_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
