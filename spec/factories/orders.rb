e696d6ba2673b0040acbb531d2e7713256bdff81
anton <tehanovanton@gmail.com>
2021-09-09 23:37:05 +0300

    factory can get params for invalid object

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -4,8 +4,12 @@ FactoryBot.define do
   factory :order do
     description { Faker::Coffee.blend_name }
     association :client
-    association :mechanic
-    service_order_attributes { attributes_for(:service_order, service_id: mechanic.services.first.id) }
+    mechanic { association :mechanic, service_id: service_id }
+    service_order_attributes { attributes_for(:service_order, service_id: service_id) }
+
+    transient do
+      service_id { create(:service).id }
+    end
 
     trait :rubber_replacement do
       service_id { create(:mechanic, :rubber_replacement).services.first.id }
