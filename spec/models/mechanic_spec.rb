be420d4c1b74f2fd8187a889acad62708ef3baef
anton <tehanovanton@gmail.com>
2021-09-10 17:54:45 +0300

    mechanic tests refactored

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe Mechanic, type: :model do
   let(:mechanic) { FactoryBot.create(:mechanic) }
   let(:mechanic_without_services) { FactoryBot.build(:mechanic, service_id: nil) }
 
-  it 'should have at least one service or be invalid' do
+  it 'should have at least one service' do
     expect(mechanic.services).not_to be_empty
+  end
+
+  it 'is invalid without services' do
     expect(mechanic_without_services).not_to be_valid
   end
 end
