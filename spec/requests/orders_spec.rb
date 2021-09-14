2459c63c512cf7f2d9e5ef5b0e3f98758181d413
anton <tehanovanton@gmail.com>
2021-09-14 23:53:33 +0300

    orders controller tests

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -4,20 +4,11 @@ require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
   before :each do
+    @order = FactoryBot.create(:order)
     @user = FactoryBot.create(:user)
     @user.confirm
     @params = { locale: I18n.locale, client_id: @user.id }
     sign_in @user
-  end
-
-  before 'POST create' do
-    @order = FactoryBot.create(:order)
-    @order_params = { locale: I18n.locale, order: {
-      client_id: @order.client.id,
-      description: @order.description,
-      service_order_attributes: { service_id: @order.service.id },
-      mechanic_id: @order.mechanic.id
-    }, client_id: @order.client.id }
   end
 
   describe 'GET index' do
@@ -65,13 +56,78 @@ RSpec.describe OrdersController, type: :controller do
 
   describe 'POST create' do
     it 'has a 302 status code' do
-      post :create, params: @order_params
+      @params[:order] = {
+        client_id: @order.client.id,
+        description: @order.description,
+        service_order_attributes: { service_id: @order.service.id },
+        mechanic_id: @order.mechanic.id
+      }
+      post :create, params: @params
+      expect(response.status).to eq(302)
+    end
+
+    it 'redirects to show_actual_orders_path if valid order' do
+      @params[:order] = {
+        client_id: @order.client.id,
+        description: @order.description,
+        service_order_attributes: { service_id: @order.service.id },
+        mechanic_id: @order.mechanic.id
+      }
+      post :create, params: @params
+      expect(response).to redirect_to(show_actual_orders_path)
+    end
+  end
+
+  describe 'GET edit' do
+    it 'has a 200 status code' do
+      @params[:id] = @order.id
+      get :edit, params: @params
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders edit' do
+      @params[:id] = @order.id
+      get :edit, params: @params
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'POST update' do
+    it 'has a 302 status code' do
+      @params[:order] = {
+        client_id: @order.client.id,
+        description: @order.description,
+        service_order_attributes: { service_id: @order.service.id },
+        mechanic_id: @order.mechanic.id
+      }
+      @params[:id] = @order.id
+      post :update, params: @params
+      expect(response.status).to eq(302)
+    end
+
+    it 'redirects to show_actual_orders_path if valid order' do
+      @params[:order] = {
+        client_id: @order.client.id,
+        description: @order.description,
+        service_order_attributes: { service_id: @order.service.id },
+        mechanic_id: @order.mechanic.id
+      }
+      @params[:id] = @order.id
+      post :update, params: @params
+      expect(response).to redirect_to(show_actual_orders_path)
+    end
+  end
+
+  describe 'POST destroy' do
+    it 'has a 302 status code' do
+      @params[:id] = @order.id
+      post :destroy, params: @params
       expect(response.status).to eq(302)
     end
 
     it 'redirects to show_actual_orders_path' do
-      post :create, params: @order_params
-
+      @params[:id] = @order.id
+      post :destroy, params: @params
       expect(response).to redirect_to(show_actual_orders_path)
     end
   end
