321355b922546bb96cf275876290cc421b045b00
anton <tehanovanton@gmail.com>
2021-09-26 17:43:30 +0300

    users tests fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,28 +4,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.create(:user) }
-  let(:user_empty_first_name) { FactoryBot.build(:user, first_name: '') }
-  let(:user_empty_last_name) { FactoryBot.build(:user, last_name: '') }
-  let(:user_empty_email) { FactoryBot.build(:user, email: '') }
-  let(:user_empty_phone_number) { FactoryBot.build(:user, phone_number: '') }
+  let(:user_with_empty_first_name) { FactoryBot.build(:user, first_name: '') }
+  let(:user_with_empty_last_name) { FactoryBot.build(:user, last_name: '') }
+  let(:user_with_empty_email) { FactoryBot.build(:user, email: '') }
+  let(:user_with_empty_phone_number) { FactoryBot.build(:user, phone_number: '') }
 
   it 'should have first name or be invalid' do
     expect(user.first_name).not_to eq('')
-    expect(user_empty_first_name).not_to be_valid
   end
+  it 'invalid without first_name' do
+    expect(user_with_empty_first_name).not_to be_valid
+  end
+
 
   it 'should have last name or be invalid' do
-    expect(user.last_name).not_to eq('')
-    expect(user_empty_last_name).not_to be_valid
+    expect(user.last_name).not_to eq('')    
   end
+  it 'invalid without last_name' do
+    expect(user_with_empty_last_name).not_to be_valid
+  end
+
+
 
   it 'should have email or be invalid' do
-    expect(user.email).not_to eq('')
-    expect(user_empty_email).not_to be_valid
+    expect(user.email).not_to eq('')  
   end
+  it 'invalid without email' do
+    expect(user_with_empty_email).not_to be_valid
+  end
+  
 
   it 'should have phone_number' do
-    expect(user.phone_number).not_to eq('')
-    expect(user_empty_phone_number).not_to be_valid
+    expect(user.phone_number).not_to eq('')    
+  end
+  it 'invalid without phone_number' do
+    expect(user_with_empty_phone_number).not_to be_valid
   end
 end
