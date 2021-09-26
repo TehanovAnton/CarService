4fdd8d5855eba1acdce2a1d063fa47c354ad95e8
anton <tehanovanton@gmail.com>
2021-09-26 17:34:45 +0300

    order tests fix

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,17 +4,24 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   let(:service) { FactoryBot.create(:service) }
-  let(:order_with_specific_service)  { FactoryBot.create(:order, service_id: service.id) }
+  let(:order_with_specific_service) { FactoryBot.create(:order, service_id: service.id) }
+
+  let(:mechanic) { FactoryBot.create(:mechanic) }
+  let(:order_with_specific_mechahic) { FactoryBot.create(:order, mechanic: mechanic, service_id: mechanic.services.first.id) }
+
+  let(:client) { FactoryBot.create(:client) }
+  let(:order_with_specific_client) { FactoryBot.create(:order, client: client) }  
+
   let(:order) { FactoryBot.create(:order) }
-  let(:order_empty_description) { FactoryBot.build(:order, description: '') }
-  let(:order_mechanic_cant_service) { FactoryBot.build(:order, mechanic: FactoryBot.create(:mechanic)) }
+  let(:order_with_empty_description) { FactoryBot.build(:order, description: '') }
+  let(:order_with_unsupproted_service) { FactoryBot.build(:order, mechanic: FactoryBot.create(:mechanic)) }
 
   it 'should have description' do
     expect(order.description).not_to be_blank
   end
 
   it 'is invalid with empty description' do
-    expect(order_empty_description).not_to be_valid
+    expect(order_with_empty_description).not_to be_valid
   end
 
   it 'should have mechanic' do
@@ -29,11 +36,19 @@ RSpec.describe Order, type: :model do
     expect(order.mechanic.services).to include(order.service)
   end
 
-  it 'is invalid with mechanic that can\'t do choosen service' do
-    expect(order_mechanic_cant_service).not_to be_valid
+  it 'is invalid with unsupported service' do
+    expect(order_with_unsupproted_service).not_to be_valid
   end
 
   it 'service stored corectly' do
     expect(order_with_specific_service.service).to be == service
+  end
+
+  it 'mechanic stored corectly' do
+    expect(order_with_specific_mechahic.mechanic).to be == mechanic
+  end
+
+  it 'client stored corectly' do
+    expect(order_with_specific_client.client).to be == client
   end
 end
