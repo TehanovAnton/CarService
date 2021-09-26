3e1c112da29f976e18201439f34ae85123e5c481
anton <tehanovanton@gmail.com>
2021-09-26 17:25:23 +0300

    service order stored corectly test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  let(:service) { FactoryBot.create(:service) }
+  let(:order_with_specific_service)  { FactoryBot.create(:order, service_id: service.id) }
   let(:order) { FactoryBot.create(:order) }
   let(:order_empty_description) { FactoryBot.build(:order, description: '') }
   let(:order_mechanic_cant_service) { FactoryBot.build(:order, mechanic: FactoryBot.create(:mechanic)) }
 
-  it 'should have not empty description' do
-    expect(order.description).not_to eq('')
+  it 'should have description' do
+    expect(order.description).not_to be_blank
   end
 
   it 'is invalid with empty description' do
@@ -29,5 +31,9 @@ RSpec.describe Order, type: :model do
 
   it 'is invalid with mechanic that can\'t do choosen service' do
     expect(order_mechanic_cant_service).not_to be_valid
+  end
+
+  it 'service stored corectly' do
+    expect(order_with_specific_service.service).to be == service
   end
 end
